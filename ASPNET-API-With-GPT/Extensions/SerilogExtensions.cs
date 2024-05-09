7aae50e42de4f680240c49431678bfd364c4fe6c
Renan Hatherly <renan.hath@gmail.com>
2024-05-08 22:02:16 -0300

    SerilogExtensions added
    - A class for extending Serilog logging was added.

---
+++ ASPNET-API-With-GPT/Extensions/SerilogExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+
+namespace ASPNET_API_With_GPT.Extensions
+{
+    public static class SerilogExtensions
+    {
+        public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder,
+                                                       IConfiguration configuration,
+                                                       string appName)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            builder.Logging.ClearProviders();
+            builder.Host.UseSerilog(Log.Logger, true);
+
+            return builder;
+        }
+    }
+}
--- ASPNET-API-With-GPT/Program.cs
+++ ASPNET-API-With-GPT/Program.cs
@@ -1,6 +1,7 @@
 using ASPNET_API_With_GPT.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.AddSerilog(builder.Configuration, "ASP.NET Web API with ChatGPT");
 builder.AddChatGpt();
 
 // Add services to the container.
